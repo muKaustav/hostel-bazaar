46cc58e82917f2192067401b3e6585c78da01a62
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2023-01-10 02:31:56 +0530

    add fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,49 +1,38 @@
 module.exports = {
-    apps: [
-        {
-            name: "admin",
-            cwd: "./admin-service/server.js",
-            script: "npm",
-            args: "start",
-            env: {
-                NODE_ENV: "production"
-            }
-        },
-        {
-            name: "auth",
-            cwd: "./auth-service/server.js",
-            script: "npm",
-            args: "start",
-            env: {
-                NODE_ENV: "production"
-            }
-        },
-        {
-            name: "order",
-            cwd: "./order-service/server.js",
-            script: "npm",
-            args: "start",
-            env: {
-                NODE_ENV: "production"
-            }
-        },
-        {
-            name: "product",
-            cwd: "./product-service/server.js",
-            script: "npm",
-            args: "start",
-            env: {
-                NODE_ENV: "production"
-            }
-        },
-        {
-            name: "profile",
-            cwd: "./profile-service/server.js",
-            script: "npm",
-            args: "start",
-            env: {
-                NODE_ENV: "production"
-            }
+    apps: [{
+        name: "admin",
+        script: "./admin-service/server.js",
+        watch: true,
+        env: {
+            NODE_ENV: "production"
         }
-    ]
+    }, {
+        name: "auth",
+        script: "./auth-service/server.js",
+        watch: true,
+        env: {
+            NODE_ENV: "production"
+        }
+    }, {
+        name: "order",
+        script: "./order-service/server.js",
+        watch: true,
+        env: {
+            NODE_ENV: "production"
+        }
+    }, {
+        name: "product",
+        script: "./product-service/server.js",
+        watch: true,
+        env: {
+            NODE_ENV: "production"
+        }
+    }, {
+        name: "profile",
+        script: "./profile-service/server.js",
+        watch: true,
+        env: {
+            NODE_ENV: "production"
+        }
+    }]
 }
