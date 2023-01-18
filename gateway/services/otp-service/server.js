64ae1521327d51b88ba823cd43f23b82606aabd8
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2023-01-18 21:34:23 +0530

    add otp service

---
+++ gateway/services/otp-service/server.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+const express = require('express')
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const client = require('twilio')(accountSid, authToken)
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/otp/send-otp', (req, res) => {
+    let number = req.body.number
+
+    try {
+        client.verify.v2.services(process.env.TWILIO_VERIFY_SERVICE_SID)
+            .verifications
+            .create({ to: number, channel: 'whatsapp' })
+            .then(verification => {
+                console.log(verification.accountSid)
+                res.json({
+                    message: 'OTP sent successfully',
+                    status: verification.status
+                })
+            })
+    } catch (err) {
+        console.log(err)
+        res.json({
+            message: 'Error sending OTP',
+            status: 'failed'
+        })
+    }
+
+})
+
+app.get('/otp/verify-otp', (req, res) => {
+    let number = req.body.number
+    let otp = req.body.otp
+
+    try {
+        client.verify.v2.services(process.env.TWILIO_VERIFY_SERVICE_SID)
+            .verificationChecks
+            .create({ to: number, code: otp })
+            .then(verification_check => {
+                console.log(verification_check.status)
+                res.json({ message: 'OTP verified successfully' })
+            })
+    } catch (err) {
+        console.log(err)
+        res.json({
+            message: 'Error verifying OTP',
+            status: 'failed'
+        })
+    }
+})
+
+let PORT = process.env.PORT || 5006
+
+app.listen(5006, () => {
+    console.log(`OTP service is running on port ${PORT}`)
+})
