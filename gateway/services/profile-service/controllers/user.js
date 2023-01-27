7552824c22a31ef3ec294bf4deb47d0162707806
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2023-01-27 13:16:50 +0530

    edit order route

--- gateway/services/profile-service/controllers/user.js
+++ gateway/services/profile-service/controllers/user.js
@@ -10,13 +10,96 @@ let getUsers = (req, res) => {
         })
     }
 
-    UserModel.find({}, (err, users) => {
-        if (err) {
-            res.send(err)
-        } else {
-            res.send(users)
-        }
-    })
+    try {
+        UserModel.find({}, (err, users) => {
+            if (err) {
+                res.send(err)
+            } else {
+                res.send(users)
+            }
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        })
+    }
 }
 
-module.exports = { getUsers }
+let getUser = (req, res) => {
+    let user = req.user
+
+    try {
+        UserModel.findOne({ _id: user._id }, (err, user) => {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    message: 'Internal Server Error'
+                })
+            }
+
+            return res.status(200).json({
+                success: true,
+                message: 'User found.',
+                user: user
+            })
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        })
+    }
+}
+
+let editUser = (req, res) => {
+    let user = req.user
+
+    try {
+        UserModel.findOneAndUpdate({ _id: user._id }, req.body, { new: true }, (err, user) => {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    message: 'Internal Server Error'
+                })
+            }
+
+            return res.status(200).json({
+                success: true,
+                user: user
+            })
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        })
+    }
+}
+
+let deleteUser = (req, res) => {
+    let user = req.user
+
+    try {
+        UserModel.findOneAndDelete({ _id: user._id }, (err, user) => {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    message: 'Internal Server Error'
+                })
+            }
+            
+            return res.status(200).json({
+                success: true,
+                message: 'User deleted.'
+            })
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        })
+    }
+}
+
+module.exports = { getUsers, getUser, editUser, deleteUser }
