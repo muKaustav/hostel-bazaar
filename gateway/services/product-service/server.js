fff63d885b81c77acb4dfd9be9c6c83d8094bf4b
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2023-01-30 16:10:02 +0530

    review add

--- gateway/services/product-service/server.js
+++ gateway/services/product-service/server.js
@@ -12,8 +12,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/product', productRoutes)
 app.use('/product/review', reviewRoutes)
+app.use('/product', productRoutes)
 
 PORT = process.env.PORT || 5002
 
