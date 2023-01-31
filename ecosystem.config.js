8900b2ffa5d24689f5f1e6130a229d9e849b5346
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2023-01-31 19:46:34 +0530

    add creds

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -90,7 +90,8 @@ module.exports = {
         watch: true,
         env: {
             AWS_ACCESS_KEY_ID: 'AKIATUAFIM7HIPMZNWAU',
-            AWS_SECRET_ACCESS_KEY: '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'
+            AWS_SECRET_ACCESS_KEY: 'bs0jb+YxFy2SYf/hgK6Q0UMtoR0kMQyjRMRNx7P4',
+            ACCESS_TOKEN_SECRET: '8fbf236d964f9a57cee7712bb49c88b9300937f0265d125732807b44e1843fe8'
         }
     }, {
         name: "gateway",
