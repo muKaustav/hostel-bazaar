78aa7e1fbe04db8b1aab769f46bc747af386744e
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2023-02-05 15:52:39 +0530

    add version route

---
+++ gateway/services/admin-service/routes/version.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the Hostel-Bazaar Admin Service',
+        time: new Date().toLocaleString(),
+        ip: req.ip
+    })
+})
+
+router.get('/current', (req, res) => {
+    res.json({
+        version: '1.0.0+1'
+    })
+})
+
+router.get('*', (req, res) => {
+    res.redirect('/admin')
+})
+
+module.exports = router
--- gateway/services/admin-service/server.js
+++ gateway/services/admin-service/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const hostelRoutes = require('./routes/hostel')
 const collegeRoutes = require('./routes/college')
 const categoryRoutes = require('./routes/category')
+const versionRoutes = require('./routes/version')
 require('./auth/passport')
 
 const app = express()
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/admin/hostel', hostelRoutes)
 app.use('/admin/college', collegeRoutes)
 app.use('/admin/category', categoryRoutes)
+app.use('/admin/version', versionRoutes)
 
 PORT = process.env.PORT || 5005
 
